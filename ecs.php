3fbe00caa0d32f40822a0ecef9e6b3e8a226bfe2
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2025-07-09 09:52:39 -0400

    fix: Add `case` to the ordered class elements in ECS configuration. (#63)

--- ecs.php
+++ ecs.php
@@ -26,6 +26,7 @@ return ECSConfig::configure()
                 'constant_public',
                 'constant_protected',
                 'constant_private',
+                'case',
                 'property_public',
                 'property_protected',
                 'property_private',
